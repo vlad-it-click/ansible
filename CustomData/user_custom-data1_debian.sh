313d04a161f2bb9856e6e41398d8d076951315f1
Vlad Voloshyn <vladreamer@gmail.com>
2025-01-19 16:08:16 -0500

    update 4

--- CustomData/user_custom-data1_debian.sh
+++ CustomData/user_custom-data1_debian.sh
@@ -6,7 +6,9 @@ apt -y install nginx procps net-tools curl vim
 cat <<EOF > /var/www/html/index.html
 <html>
 <body bgcolor="blue">
-<h2><font color="red">Build by CustomData/UserData</font></h2><br><p> 
+<CENTER>
+<br><br><br><br>
+<h2><font color="Light gray">Build by CustomData/UserData</font></h2><br><p> 
 <h2>WebServer: $(hostname) ($(hostname -I | awk '{print $1}')) created:  $(date '+%Y-%m-%d %H:%M:%S')  by user:  $(whoami) </h2><br>
 <h2>It has: MEMORY - $(free -h | awk '/^Mem:/ {print $2}');  DISK SIZE -  $(df -h / | awk 'NR==2 {print $2}')</h2><br><br>
 <font color="yellow"><b> Version 1.0 </b>
